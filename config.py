6859375f0d9ff6856bdeeee11aa59f974be214e7
Kenyi001 <daxkenyi001@gmail.com>
2023-12-13 16:18:48 -0400

    docs: Añadir documentación inicial y guía de configuración
    Este commit introduce documentación esencial para el proyecto, que incluye:
    
    - README.md con descripción general del proyecto, requisitos previos, y pasos detallados para la instalación y ejecución.
    
    Además, se han realizado las siguientes actualizaciones al código:
    - Refactorización del script de automatización para mejorar la claridad y mantenibilidad.
    - Implementación de manejo de excepciones para capturar y registrar errores durante la ejecución del WebDriver.
    - Creación de un archivo de configuración para centralizar la gestión de rutas y variables de entorno.
    - Actualización de las rutas de WebDriver y perfiles de usuario para soportar ejecución en diferentes entornos.
    
    Estos cambios mejoran la usabilidad y la colaboración en el proyecto, y proporcionan una base sólida para futuras contribuciones y desarrollos.

--- config.py
+++ config.py
@@ -6,10 +6,11 @@ WEBDRIVER_PATH = os.getenv('EDGE_PATH')
 EDGE_BINARY_PATH = os.getenv('EDGE_DRIVER_PATH')
 USER_PROFILE_PATH = os.getenv('EDGE_PROFILE_PATH')
 
-print(f"WebDriver Path: {WEBDRIVER_PATH}")
-print(f"Edge Binary Path: {EDGE_BINARY_PATH}")
-print(f"User Profile Path: {USER_PROFILE_PATH}")
-
 # Asegúrate de que las variables no estén vacías
 if not all([WEBDRIVER_PATH, EDGE_BINARY_PATH, USER_PROFILE_PATH]):
-    raise ValueError("Una o más variables de entorno requeridas no están definidas.")
+    # Configuración de las rutas para el WebDriver y el perfil de usuario
+    WEBDRIVER_PATH = r'C:\msedgedriver\msedgedriver.exe'
+    EDGE_BINARY_PATH = r'C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe'
+    USER_PROFILE_PATH = r'C:\Users\tellezd\AppData\Local\Microsoft\Edge\User Data\Default'
+
+    # raise ValueError("Una o más variables de entorno requeridas no están definidas.")
